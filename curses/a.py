1e59c63c0d53ed1449b0fd7781fd30bc3cf2140f
zhbmath <zhbmath@163.com>
2018-01-22 16:01:50 +0800

    add number navigator

--- curses/a.py
+++ curses/a.py
@@ -18,15 +18,48 @@ def get_ch(win):
     win.nodelay(1) #重置非阻塞式接受输入，超时1秒
     return ch
 
+def show_msg(win, x=10, y=10, msg="hello"):
+    win.addstr(y, x, msg)
+    win.refresh()
+
+def move_cursor(win, x=10, y=10):
+    win.move(y, x)
+    win.refresh()
+
+def loop(m,n):
+    if n<m:  
+        return n
+    else:
+        return n%m
+
+def navigate(position, total, n=1):
+    """A navigator."""
+    position += n
+    if position < 0:
+        position = total-1 
+    elif position > total-1:
+        position = 0
+    return position
+
 def main():
     stdscr=init_screen() 
+    show_msg(stdscr, curses.COLS/2, curses.LINES/2+0, "[   ] netcat" )
+    show_msg(stdscr, curses.COLS/2, curses.LINES/2+1, "[   ] tree" )
+    show_msg(stdscr, curses.COLS/2, curses.LINES/2+2, "[   ] vim" )
+    show_msg(stdscr, curses.COLS/2, curses.LINES/2+3, "[   ] pv" )
+    move_cursor(stdscr, curses.COLS/2+2, curses.LINES/2)
     stdscr.keypad(1) # 打开扩展键盘
+    pos=0
     while True:
         ch=get_ch(stdscr)
         if ( ch==ord('q') or ch==curses.ascii.ESC or ch==112 ):
             break
+        if ( ch==curses.KEY_UP ):
+	    pos=navigate(pos, 4, -1)
+            move_cursor(stdscr, curses.COLS/2+2, curses.LINES/2+pos )
         if ( ch==curses.KEY_DOWN ):
-            stdscr.addstr("Hello,")
+	    pos=navigate(pos, 4, 1)
+            move_cursor(stdscr, curses.COLS/2+2, curses.LINES/2+pos )
     
 if __name__=='__main__':
     try:
